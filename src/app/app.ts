ffd0caf60b7ce64718d9ff870412aa7b0720ef87
Cauê Soares Valente <caue.valente@cptm.sp.gov.br>
2025-08-04 10:15:52 -0300

    Componentes Blog

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BigCard} from './componentes/big-card/big-card.js';
+import {MenuBar} from './componentes/menu-bar/menu-bar.js';
+import {MenuTitle} from './componentes/menu-title/menu-title.js';
+import {SmallCard} from './componentes/small-card/small-card.js';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BigCard, MenuBar, MenuTitle, SmallCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
