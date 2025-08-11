650870cf4bd9c5d33f8efdc6734f9b1e6febf138
Cauê Soares Valente <caue.valente@cptm.sp.gov.br>
2025-08-11 14:44:29 -0300

    Projeto Final

---
+++ src/app/dados/dadosFake.ts
@@ -0,0 +1,15 @@
+export const dadosFake = [
+    {
+        "id":1,
+        "photo": "https://jpimg.com.br/uploads/2024/01/aniversario-de-sao-paulo-10-curiosidades-sobre-a-cidade.jpg",
+        "title": "saiu novo filme",
+        "description": "olá"
+    },
+       {
+        "id":2,
+        "photo": "https://yt3.googleusercontent.com/HxdDIohuLXL7E2JpK97Xm1d_P4Qosm2SwnZzDWrelcBSctGglF2UPaT215JkoxSi3o1-aUgieNk=s900-c-k-c0x00ffffff-no-rj",
+        "title": "saiu a nova série na netflix",
+        "description": "Tudo bem"
+    },
+
+]
--- src/app/pages/content/content.ts
+++ src/app/pages/content/content.ts
@@ -1,17 +1,44 @@
 import { Component, Input} from '@angular/core';
-import { RouterLink} from '@angular/router';
+import { ActivatedRoute, RouterLink} from '@angular/router';
+import { dadosFake } from '../../dados/dadosFake';
+
 @Component({
   selector: 'app-content',
-  imports: [RouterLink],
+  imports: [RouterLink,],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
 export class Content {
+
   @Input()
   fotocontent:string = "https://ssl.gstatic.com/onebox/media/sports/logos/4w2Z97Hf9CSOqICK3a8AxQ_64x64.png"
   @Input()
   tituloContentontent:string = "São Paulo Futebol Clube"
   @Input()
   paragrafoContent:string = "São Paulo Futebol Clube"
+  private id:string | null = "0"
+
+  constructor(
+   private route:ActivatedRoute
+  ){}
+    
+   ngOnInit(): void{
+    
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setDadosPeloId(this.id)
+   }
+  
+  setDadosPeloId(id:string | null ){
+    const resul = dadosFake.filter(article => article.id.toString() == id)[0]
+ 
+    this.tituloContentontent = resul.title
+    this.paragrafoContent = resul.description
+    this.fotocontent = resul.photo
+  }
+
+  
 
 }
